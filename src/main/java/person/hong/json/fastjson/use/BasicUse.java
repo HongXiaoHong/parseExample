1dea31898f4ffa45c017f8553b801e0b3d417b61
HongXiaoHong <1908711045@qq.com>
2020-09-20 15:12:24 +0800

    加入test依赖到pom文件中

--- src/main/java/person/hong/json/fastjson/use/BasicUse.java
+++ src/main/java/person/hong/json/fastjson/use/BasicUse.java
@@ -1,38 +1,36 @@
 package person.hong.json.fastjson.use;
 
-import org.junit.Test;
-
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-
+import org.junit.jupiter.api.Test;
 import person.hong.json.fastjson.entity.Student;
 
 
 public class BasicUse {
 
-	//json字符串-简单对象型
-	private static final String  JSON_OBJ_STR = "{\"studentName\":\"lily\",\"studentAge\":12}";
+    //json字符串-简单对象型
+    private static final String JSON_OBJ_STR = "{\"studentName\":\"lily\",\"studentAge\":12}";
 
-	//json字符串-数组类型
-	private static final String  JSON_ARRAY_STR = "[{\"studentName\":\"lily\",\"studentAge\":12},{\"studentName\":\"lucy\",\"studentAge\":15}]";
+    //json字符串-数组类型
+    private static final String JSON_ARRAY_STR = "[{\"studentName\":\"lily\",\"studentAge\":12},{\"studentName\":\"lucy\",\"studentAge\":15}]";
 
-	//复杂格式json字符串
-	private static final String  COMPLEX_JSON_STR = "{\"teacherName\":\"crystall\",\"teacherAge\":27,\"course\":{\"courseName\":\"english\",\"code\":1270},\"students\":[{\"studentName\":\"lily\",\"studentAge\":12},{\"studentName\":\"lucy\",\"studentAge\":15}]}";
-	
-	@Test
-	public void sampleJson() {
-		Student stu = JSONObject.parseObject(JSON_OBJ_STR, Student.class);
-		System.out.println(stu);
-		
-		String stuJson = JSONObject.toJSONString(stu);
-		System.out.println(stuJson);
-		
-		JSONObject jsonObject = JSONObject.parseObject(JSON_OBJ_STR);
-		System.out.println(jsonObject);
-		
-		String objJson = JSONObject.toJSONString(jsonObject);
-		System.out.println(objJson);
-	}
+    //复杂格式json字符串
+    private static final String COMPLEX_JSON_STR = "{\"teacherName\":\"crystall\",\"teacherAge\":27,\"course\":{\"courseName\":\"english\",\"code\":1270},\"students\":[{\"studentName\":\"lily\",\"studentAge\":12},{\"studentName\":\"lucy\",\"studentAge\":15}]}";
+
+    @Test
+    public void sampleJson() {
+        Student stu = JSONObject.parseObject(JSON_OBJ_STR, Student.class);
+        System.out.println(stu);
+
+        String stuJson = JSONObject.toJSONString(stu);
+        System.out.println(stuJson);
+
+        JSONObject jsonObject = JSONObject.parseObject(JSON_OBJ_STR);
+        System.out.println(jsonObject);
+
+        String objJson = JSONObject.toJSONString(jsonObject);
+        System.out.println(objJson);
+    }
 	/*
 	 * 结果 :)
 	 Student [studentName=lily, studentAge=12]
@@ -40,79 +38,81 @@ public class BasicUse {
 	{"studentAge":12,"studentName":"lily"}
 	{"studentAge":12,"studentName":"lily"}
 	 */
-	
-	@Test
-	public void arrsyJson() {
-		// 序列化
-		JSONArray jsonArray = JSONObject.parseArray(JSON_ARRAY_STR);
-		for (int i=0; i<jsonArray.size(); i++) {
-			JSONObject jsonObj = jsonArray.getJSONObject(i);
-			System.out.println(jsonObj);
-		}
-		// 反序列化
-		// 因为对象的toString方法已经重写 会自动调用 toJSONString 方法
-		System.out.println(jsonArray);
-	}
+
+    @Test
+    public void arrsyJson() {
+        // 序列化
+        JSONArray jsonArray = JSONObject.parseArray(JSON_ARRAY_STR);
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            System.out.println(jsonObj);
+        }
+        // 反序列化
+        // 因为对象的toString方法已经重写 会自动调用 toJSONString 方法
+        System.out.println(jsonArray);
+    }
 	/*
 	 * 结果 :)
 	 {"studentAge":12,"studentName":"lily"}
 	{"studentAge":15,"studentName":"lucy"}
 	[{"studentAge":12,"studentName":"lily"},{"studentAge":15,"studentName":"lucy"}]
 	 */
-	/**
-	 * 复杂json格式字符串到JSONObject的转换
-	 */
-	@Test
-	public void testComplexJSONStrToJSONObject() {
 
-	    JSONObject jsonObject = JSONObject.parseObject(COMPLEX_JSON_STR);
+    /**
+     * 复杂json格式字符串到JSONObject的转换
+     */
+    @Test
+    public void testComplexJSONStrToJSONObject() {
 
-	    String teacherName = jsonObject.getString("teacherName");
-	    Integer teacherAge = jsonObject.getInteger("teacherAge");
+        JSONObject jsonObject = JSONObject.parseObject(COMPLEX_JSON_STR);
 
-	    System.out.println("teacherName:  " + teacherName + "   teacherAge:  " + teacherAge);
+        String teacherName = jsonObject.getString("teacherName");
+        Integer teacherAge = jsonObject.getInteger("teacherAge");
 
-	    JSONObject jsonObjectcourse = jsonObject.getJSONObject("course");
-	     //获取JSONObject中的数据
-	    String courseName = jsonObjectcourse.getString("courseName");
-	    Integer code = jsonObjectcourse.getInteger("code");
+        System.out.println("teacherName:  " + teacherName + "   teacherAge:  " + teacherAge);
 
-	    System.out.println("courseName:  " + courseName + "   code:  " + code);
+        JSONObject jsonObjectcourse = jsonObject.getJSONObject("course");
+        //获取JSONObject中的数据
+        String courseName = jsonObjectcourse.getString("courseName");
+        Integer code = jsonObjectcourse.getInteger("code");
 
-	    JSONArray jsonArraystudents = jsonObject.getJSONArray("students");
+        System.out.println("courseName:  " + courseName + "   code:  " + code);
 
-	    //遍历JSONArray
-	    for (Object object : jsonArraystudents) {
+        JSONArray jsonArraystudents = jsonObject.getJSONArray("students");
 
-	        JSONObject jsonObjectone = (JSONObject) object;
-	        String studentName = jsonObjectone.getString("studentName");
-	        Integer studentAge = jsonObjectone.getInteger("studentAge");
+        //遍历JSONArray
+        for (Object object : jsonArraystudents) {
 
-	        System.out.println("studentName:  " + studentName + "   studentAge:  " + studentAge);
-	    }
-	}
+            JSONObject jsonObjectone = (JSONObject) object;
+            String studentName = jsonObjectone.getString("studentName");
+            Integer studentAge = jsonObjectone.getInteger("studentAge");
+
+            System.out.println("studentName:  " + studentName + "   studentAge:  " + studentAge);
+        }
+    }
 	/* 结果 :)
 	courseName:  english   code:  1270
 	studentName:  lily   studentAge:  12
 	studentName:  lucy   studentAge:  15
 	 */
-	/**
-	 * 复杂JSONObject到json格式字符串的转换
-	 */
-	@Test
-	public void testJSONObjectToComplexJSONStr() {
 
-	   //复杂JSONObject,目标要转换为json字符串
-	    JSONObject jsonObject = JSONObject.parseObject(COMPLEX_JSON_STR);
+    /**
+     * 复杂JSONObject到json格式字符串的转换
+     */
+    @Test
+    public void testJSONObjectToComplexJSONStr() {
 
-	    //第一种方式
-	    //String jsonString = JSONObject.toJSONString(jsonObject);
+        //复杂JSONObject,目标要转换为json字符串
+        JSONObject jsonObject = JSONObject.parseObject(COMPLEX_JSON_STR);
 
-	    //第二种方式
-	    String jsonString = jsonObject.toJSONString();
-	    System.out.println(jsonString);
+        //第一种方式
+        //String jsonString = JSONObject.toJSONString(jsonObject);
 
-	}
+        //第二种方式
+        String jsonString = jsonObject.toJSONString();
+        System.out.println(jsonString);
+
+    }
 	/* 结果 :)
 	{"teacherAge":27,"teacherName":"crystall","course":{"courseName":"english","code":1270},"students":[{"studentAge":12,"studentName":"lily"},{"studentAge":15,"studentName":"lucy"}]}
 	 */
